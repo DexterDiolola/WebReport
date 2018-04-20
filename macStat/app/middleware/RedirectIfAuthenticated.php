2f4f7e8d38bd6cb6d95c17e617b023b85cc23a47
DexterDiolola <dexterdiolola@gmail.com>
2018-04-20 11:50:22 +0800

    Create RedirectIfAuthenticated.php

---
+++ macStat/app/middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        //if (Auth::guard($guard)->check()) {
+          //  return redirect('/user/reports');
+        //}
+
+        switch ($guard) {
+            case 'admin':
+                if(Auth::guard($guard)->check()){
+                    return redirect()->route('admin.dashboard');  //Same for using return redirect('my_url');
+                }
+                break;
+            
+            default:
+                if(Auth::guard($guard)->check()){
+                    return redirect()->route('user.dashboard');
+                }
+                break;
+        }
+
+        return $next($request);
+    }
+}
