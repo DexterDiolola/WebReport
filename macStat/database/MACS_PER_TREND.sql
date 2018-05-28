05c5ccadc586bb33cf807ca9a01c6def0d86e359
DexterDiolola <dexterdiolola@gmail.com>
2018-05-28 13:16:47 +0800

    Update MACS_PER_TREND.sql

--- macStat/database/MACS_PER_TREND.sql
+++ macStat/database/MACS_PER_TREND.sql
@@ -96,6 +96,27 @@ BEGIN
         WHERE tempUtilizations.dateCreated > DATE_SUB(NOW(), INTERVAL 1 MONTH) 
         ORDER BY tempUtilizations.dateCreated DESC;
 
+    ELSEIF trend = "alert" THEN
+        INSERT INTO tempUtilizations(mac, active, utiltx,
+        utilrx, usagetx, usagerx, ccq, lease, uptime, 
+        freeMem, cpuFreq, cpuLoad, freeHdd, badBlock, version, appVersion,
+        gps, dispense, packages, vpnaddr, dateCreated, dateUpdated)
+            SELECT mac_fk, MAX(active), MAX(utiltx), MAX(utilrx), 
+            MAX(usagetx), MAX(usagerx), MAX(ccq), MAX(lease), 
+            uptime, MAX(freeMemory), MAX(cpuFreq), MAX(cpuLoad), 
+            MAX(freeHdd), MAX(badBlock), version, appVersion, gps, dispense, 
+            packages, vpnaddr, dateCreated, dateUpdated FROM utilizations
+            WHERE  dateCreated > DATE_SUB(NOW(), INTERVAL 4 DAY)
+            GROUP BY DATE(dateCreated), mac_fk;
+        
+        SELECT tempUtilizations.mac, label, owner, active, utiltx, utilrx, 
+        usagetx, usagerx, ccq, lease, freeMem, cpuFreq, cpuLoad, 
+        freeHdd, badBlock, uptime, version, appVersion, gps, dispense, packages, 
+        vpnaddr, DATE_FORMAT(tempUtilizations.dateCreated, '%Y-%m-%d') AS dateCreated FROM tempUtilizations 
+        LEFT OUTER JOIN macs ON tempUtilizations.mac = macs.mac 
+        LEFT OUTER JOIN macs_users ON tempUtilizations.mac = macs_users.mac
+        ORDER BY tempUtilizations.dateCreated DESC;
+
     
     END IF;
     
