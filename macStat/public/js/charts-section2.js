b15025233b7941bf33b2cd41ed22a9c5adeaffbe
DexterDiolola <dexterdiolola@gmail.com>
2018-09-21 13:10:08 +0800

    Update charts-section2.js

--- macStat/public/js/charts-section2.js
+++ macStat/public/js/charts-section2.js
@@ -1,8 +1,8 @@
-var xxxMinutesBar, xxxMinutesLine, iHourBar, iHourLine, iiHoursBar, iiHoursLine,
+var xxxMinutesBar, xxxMinutesLine, iHourBar, iHourLine, iiHoursBar, iiHoursLine, iiiHoursBar, iiiHoursLine,
 	vHoursBar, vHoursLine, iDayBar, iDayLine, iiDaysBar, iiDaysLine, ivDaysBar, 
 	ivDaysLine, iWeekBar, iWeekLine;
 
-var packagesBar, packagesLine;
+var tpBar, tpLine, tdBar, tdLine;
 
 //var dateTime = [], xxxMinutes = [], iHour = [], iiHours = [], vHours = [], iDay = [], iiDays = [], ivDays = [], iWeek = [];
 
@@ -11,11 +11,10 @@ function xxxMinutesGraph(obj, trendStr){
 	var dateTime = [], xxxMinutes = [];
 
 	for(var x=0; x<obj.length; x++){
-		var spliter = obj[x]['packages'].split(",");
-		var spliter2 = obj[x]['dateCreated'].split(" ");
+		var spliter = obj[x]['dateCreated'].split(" ");
 
-		dateTime.unshift(spliter2[0]);
-		xxxMinutes.unshift(spliter[1]);
+		dateTime.unshift(spliter[0]);
+		xxxMinutes.unshift(obj[x]['xxxMinutes']);
 	}
 
 	if(xxxMinutesBar || xxxMinutesLine){
@@ -42,7 +41,7 @@ function xxxMinutesGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "30Minutes Dispensed Graph "+ trendStr,
+				text: "30Minutes Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -76,7 +75,7 @@ function xxxMinutesGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "30Minutes Dispensed Graph "+ trendStr,
+				text: "30Minutes Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -98,11 +97,10 @@ function iHourGraph(obj, trendStr){
 	var dateTime = [], iHour = [];
 
 	for(var x=0; x<obj.length; x++){
-		var spliter = obj[x]['packages'].split(",");
-		var spliter2 = obj[x]['dateCreated'].split(" ");
+		var spliter = obj[x]['dateCreated'].split(" ");
 
-		dateTime.unshift(spliter2[0]);
-		iHour.unshift(spliter[3]);
+		dateTime.unshift(spliter[0]);
+		iHour.unshift(obj[x]['iHour']);
 	}
 
 	if(iHourBar || iHourLine){
@@ -129,7 +127,7 @@ function iHourGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "1Hour Dispensed Graph "+ trendStr,
+				text: "1Hour Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -163,7 +161,7 @@ function iHourGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "1Hour Dispensed Graph "+ trendStr,
+				text: "1Hour Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -185,11 +183,10 @@ function iiHoursGraph(obj, trendStr){
 	var dateTime = [], iiHours = [];
 
 	for(var x=0; x<obj.length; x++){
-		var spliter = obj[x]['packages'].split(",");
-		var spliter2 = obj[x]['dateCreated'].split(" ");
+		var spliter = obj[x]['dateCreated'].split(" ");
 
-		dateTime.unshift(spliter2[0]);
-		iiHours.unshift(spliter[5]);
+		dateTime.unshift(spliter[0]);
+		iiHours.unshift(obj[x]['iiHours']);
 	}
 
 	if(iiHoursBar || iiHoursLine){
@@ -216,7 +213,7 @@ function iiHoursGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "2Hours Dispensed Graph "+ trendStr,
+				text: "2Hours Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -250,7 +247,7 @@ function iiHoursGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "2Hours Dispensed Graph "+ trendStr,
+				text: "2Hours Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -268,15 +265,102 @@ function iiHoursGraph(obj, trendStr){
 
 }
 
+function iiiHoursGraph(obj, trendStr){
+	var dateTime = [], iiiHours = [];
+
+	for(var x=0; x<obj.length; x++){
+		var spliter = obj[x]['dateCreated'].split(" ");
+
+		dateTime.unshift(spliter[0]);
+		iiiHours.unshift(obj[x]['iiiHours']);
+	}
+
+	if(iiiHoursBar || iiiHoursLine){
+		iiiHoursBar.destroy();
+		iiiHoursLine.destroy();
+	}
+	
+	var ctx = document.getElementById("canvas37").getContext("2d");
+	var ctx2 = document.getElementById("canvas38").getContext("2d");
+
+	iiiHoursBar = new Chart(ctx2, {
+		type: "bar",
+		data: {
+			labels: dateTime,
+			datasets: [{
+				label: "3Hours",
+				data: iiiHours,
+				backgroundColor: "rgba(255, 35, 35, 0.3)",
+				borderColor: "rgba(198, 0, 0, 0.76)",
+				borderWidth: "2"
+			}]
+		},
+		options:{
+			maintainAspectRatio: false,
+			title: {
+				display: true,
+				text: "3Hours Package "+ trendStr,
+			},
+			scales:{
+				yAxes:[{
+					ticks:{
+						beginAtZero: true,
+					}
+				}],
+				xAxes:[{
+					display: false
+				}]
+			}
+		}
+	});
+	iiiHoursLine = new Chart(ctx, {
+		type: "line",
+		data: {
+			labels: dateTime,
+			datasets: [{
+				label: "3Hours",
+				data: iiiHours,
+				backgroundColor: "rgba(255, 35, 35, 0.3)",
+				borderColor: "rgba(198, 0, 0, 0.76)",
+				borderWidth: "2",
+				lineTension: 0.3,
+	      		fill: true,
+	       		pointBackgroundColor: "rgba(198, 0, 0, 0.76)",
+	       		pointRadius: 2
+			}]
+		},
+		options:{
+			maintainAspectRatio: false,
+			title: {
+				display: true,
+				text: "3Hours Package "+ trendStr,
+			},
+			scales:{
+				yAxes:[{
+					ticks:{
+						beginAtZero: true,
+						max: Math.max(...iiiHours) + 1
+					}
+				}],
+				xAxes:[{
+					display: false
+				}]
+			}
+		}
+	});
+
+}
+
+
+
 function vHoursGraph(obj, trendStr){
 	var dateTime = [], vHours = [];
 
 	for(var x=0; x<obj.length; x++){
-		var spliter = obj[x]['packages'].split(",");
-		var spliter2 = obj[x]['dateCreated'].split(" ");
+		var spliter = obj[x]['dateCreated'].split(" ");
 
-		dateTime.unshift(spliter2[0]);
-		vHours.unshift(spliter[7]);
+		dateTime.unshift(spliter[0]);
+		vHours.unshift(obj[x]['vHours']);
 	}
 
 	if(vHoursBar || vHoursLine){
@@ -303,7 +387,7 @@ function vHoursGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "5Hours Dispensed Graph "+ trendStr,
+				text: "5Hours Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -337,7 +421,7 @@ function vHoursGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "5Hours Dispensed Graph "+ trendStr,
+				text: "5Hours Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -359,11 +443,10 @@ function iDayGraph(obj, trendStr){
 	var dateTime = [], iDay = [];
 
 	for(var x=0; x<obj.length; x++){
-		var spliter = obj[x]['packages'].split(",");
-		var spliter2 = obj[x]['dateCreated'].split(" ");
+		var spliter = obj[x]['dateCreated'].split(" ");
 
-		dateTime.unshift(spliter2[0]);
-		iDay.unshift(spliter[9]);
+		dateTime.unshift(spliter[0]);
+		iDay.unshift(obj[x]['iDay']);
 	}
 
 	if(iDayBar || iDayLine){
@@ -390,7 +473,7 @@ function iDayGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "1Day Dispensed Graph "+ trendStr,
+				text: "1Day Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -424,7 +507,7 @@ function iDayGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "1Day Dispensed Graph "+ trendStr,
+				text: "1Day Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -446,11 +529,10 @@ function iiDaysGraph(obj, trendStr){
 	var dateTime = [], iiDays = [];
 
 	for(var x=0; x<obj.length; x++){
-		var spliter = obj[x]['packages'].split(",");
-		var spliter2 = obj[x]['dateCreated'].split(" ");
+		var spliter = obj[x]['dateCreated'].split(" ");
 
-		dateTime.unshift(spliter2[0]);
-		iiDays.unshift(spliter[11]);
+		dateTime.unshift(spliter[0]);
+		iiDays.unshift(obj[x]['iiDays']);
 	}
 
 	if(iiDaysBar || iiDaysLine){
@@ -477,7 +559,7 @@ function iiDaysGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "2Days Dispensed Graph "+ trendStr,
+				text: "2Days Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -511,7 +593,7 @@ function iiDaysGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "2Days Dispensed Graph "+ trendStr,
+				text: "2Days Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -533,11 +615,10 @@ function ivDaysGraph(obj, trendStr){
 	var dateTime = [], ivDays = [];
 
 	for(var x=0; x<obj.length; x++){
-		var spliter = obj[x]['packages'].split(",");
-		var spliter2 = obj[x]['dateCreated'].split(" ");
+		var spliter = obj[x]['dateCreated'].split(" ");
 
-		dateTime.unshift(spliter2[0]);
-		ivDays.unshift(spliter[13]);
+		dateTime.unshift(spliter[0]);
+		ivDays.unshift(obj[x]['ivDays']);
 	}
 
 	if(ivDaysBar || ivDaysLine){
@@ -564,7 +645,7 @@ function ivDaysGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "4Days Dispensed Graph "+ trendStr,
+				text: "4Days Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -598,7 +679,7 @@ function ivDaysGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "4Days Dispensed Graph "+ trendStr,
+				text: "4Days Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -620,11 +701,10 @@ function iWeekGraph(obj, trendStr){
 	var dateTime = [], iWeek = [];
 
 	for(var x=0; x<obj.length; x++){
-		var spliter = obj[x]['packages'].split(",");
-		var spliter2 = obj[x]['dateCreated'].split(" ");
+		var spliter = obj[x]['dateCreated'].split(" ");
 
-		dateTime.unshift(spliter2[0]);
-		iWeek.unshift(spliter[15]);
+		dateTime.unshift(spliter[0]);
+		iWeek.unshift(obj[x]['iWeek']);
 	}
 
 	if(iWeekBar || iWeekLine){
@@ -651,7 +731,7 @@ function iWeekGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "1Week Dispensed Graph "+ trendStr,
+				text: "1Week Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -685,7 +765,7 @@ function iWeekGraph(obj, trendStr){
 			maintainAspectRatio: false,
 			title: {
 				display: true,
-				text: "1Week Dispensed Graph "+ trendStr,
+				text: "1Week Package "+ trendStr,
 			},
 			scales:{
 				yAxes:[{
@@ -703,25 +783,196 @@ function iWeekGraph(obj, trendStr){
 
 }
 
+function totalPackageGraph(obj, trendStr){
+	var dateTime = [], totalPackage = [];
 
+	for(var x=0; x<obj.length; x++){
+		var spliter = obj[x]['dateCreated'].split(" ");
+
+		dateTime.unshift(spliter[0]);
+		totalPackage.unshift(obj[x]['totalPackage']);
+	}
+
+	if(tpBar || tpLine){
+		tpBar.destroy();
+		tpLine.destroy();
+	}
+	
+	var ctx = document.getElementById("canvas39").getContext("2d");
+	var ctx2 = document.getElementById("canvas40").getContext("2d");
+
+	tpBar = new Chart(ctx2, {
+		type: "bar",
+		data: {
+			labels: dateTime,
+			datasets: [{
+				label: "Total Package",
+				data: totalPackage,
+				backgroundColor: "rgba(14, 16, 15, 0.4)",
+				borderColor: "rgba(14, 16, 15, 0.9)",
+				borderWidth: "2"
+			}]
+		},
+		options:{
+			maintainAspectRatio: false,
+			title: {
+				display: true,
+				text: "Total Package "+ trendStr,
+			},
+			scales:{
+				yAxes:[{
+					ticks:{
+						beginAtZero: true,
+					}
+				}],
+				xAxes:[{
+					display: false
+				}]
+			}
+		}
+	});
+	tpLine = new Chart(ctx, {
+		type: "line",
+		data: {
+			labels: dateTime,
+			datasets: [{
+				label: "Total Package",
+				data: totalPackage,
+				backgroundColor: "rgba(14, 16, 15, 0.4)",
+				borderColor: "rgba(14, 16, 15, 0.9)",
+				borderWidth: "2",
+				lineTension: 0.3,
+	        	fill: true,
+	        	pointBackgroundColor: "rgba(14, 16, 15, 0.9",
+	        	pointRadius: 2
+			}]
+		},
+		options:{
+			maintainAspectRatio: false,
+			title: {
+				display: true,
+				text: "Total Package "+ trendStr,
+			},
+			scales:{
+				yAxes:[{
+					ticks:{
+						beginAtZero: true,
+						max: Math.max(...totalPackage) + 1
+					}
+				}],
+				xAxes:[{
+					display: false
+				}]
+			}
+		}
+	});
+
+}
+
+function totalDispenseGraph(obj, trendStr){
+	var dateTime = [], totalDispense = [];
+
+	for(var x=0; x<obj.length; x++){
+		var spliter = obj[x]['dateCreated'].split(" ");
+
+		dateTime.unshift(spliter[0]);
+		totalDispense.unshift(obj[x]['totalDispense']);
+	}
+
+	if(tdBar || tdLine){
+		tdBar.destroy();
+		tdLine.destroy();
+	}
+	
+	var ctx = document.getElementById("canvas41").getContext("2d");
+	var ctx2 = document.getElementById("canvas42").getContext("2d");
+
+	tdBar = new Chart(ctx2, {
+		type: "bar",
+		data: {
+			labels: dateTime,
+			datasets: [{
+				label: "Total Dispense",
+				data: totalDispense,
+				backgroundColor: "rgba(170, 35, 255, 0.3)",
+				borderColor: "rgba(100, 0, 163, 0.76)",
+				borderWidth: "2"
+			}]
+		},
+		options:{
+			maintainAspectRatio: false,
+			title: {
+				display: true,
+				text: "Total Dispense "+ trendStr,
+			},
+			scales:{
+				yAxes:[{
+					ticks:{
+						beginAtZero: true,
+					}
+				}],
+				xAxes:[{
+					display: false
+				}]
+			}
+		}
+	});
+	tdLine = new Chart(ctx, {
+		type: "line",
+		data: {
+			labels: dateTime,
+			datasets: [{
+				label: "Total Dispense",
+				data: totalDispense,
+				backgroundColor: "rgba(170, 35, 255, 0.3)",
+				borderColor: "rgba(100, 0, 163, 0.76)",
+				borderWidth: "2",
+				lineTension: 0.3,
+	        	fill: true,
+	        	pointBackgroundColor: "rgba(100, 0, 163, 0.76)",
+	        	pointRadius: 2
+			}]
+		},
+		options:{
+			maintainAspectRatio: false,
+			title: {
+				display: true,
+				text: "Total Dispense "+ trendStr,
+			},
+			scales:{
+				yAxes:[{
+					ticks:{
+						beginAtZero: true,
+						max: Math.max(...totalDispense) + 1
+					}
+				}],
+				xAxes:[{
+					display: false
+				}]
+			}
+		}
+	});
+
+}
+
+/*
 function packagesGraph(obj, trendStr){
 	var dateTime = [], xxxMinutes = [], iHour = [], 
 		iiHours = [], vHours = [], iDay = [], 
 		iiDays = [], ivDays = [], iWeek = [];
 
 	for(var x=0; x<obj.length; x++){
-		var spliter = obj[x]['packages'].split(",");
-		var spliter2 = obj[x]['dateCreated'].split(" ");
+		var spliter = obj[x]['dateCreated'].split(" ");
 
-		dateTime.unshift(spliter2[0]);
-		xxxMinutes.unshift(spliter[1]);
-		iHour.unshift(spliter[3]);
-		iiHours.unshift(spliter[5]);
-		vHours.unshift(spliter[7]);
-		iDay.unshift(spliter[9]);
-		iiDays.unshift(spliter[11]);
-		ivDays.unshift(spliter[13]);
-		iWeek.unshift(spliter[15]);
+		dateTime.unshift(spliter[0]);
+		xxxMinutes.unshift(obj[x]['xxxMinutes']);
+		iHour.unshift(obj[x]['iHour']);
+		iiHours.unshift(obj[x]['iiHours']);
+		vHours.unshift(obj[x]['vHours']);
+		iDay.unshift(obj[x]['iDay']);
+		iiDays.unshift(obj[x]['iiDays']);
+		ivDays.unshift(obj[x]['ivDays']);
+		iWeek.unshift(obj[x]['iWeek']);
 	}
 
 	if(packagesBar || packagesLine){
@@ -925,3 +1176,4 @@ function packagesGraph(obj, trendStr){
 	});
 
 }
+*/
