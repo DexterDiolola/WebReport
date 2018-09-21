1a0b060274634b4442016b07f791e65483e989cb
DexterDiolola <dexterdiolola@gmail.com>
2018-09-21 13:00:39 +0800

    Update graphs.php

--- macStat/public/templates/graphs.php
+++ macStat/public/templates/graphs.php
@@ -18,6 +18,9 @@
 
 
 <div class="charts-init" ng-init="init('perDay')">
+	<div class="preloader">
+		<div class="spinner"></div>
+	</div>
 	<!--Uses the header for dashboard-->
 	<div class="db-header-frame">
 		<div class="aa row">
@@ -30,7 +33,7 @@
 						<a class="nav-link stats-tab" data-toggle="tab"><b>Stats</b></a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link packages-tab" data-toggle="tab" ng-click="packagesCharts()"><b>Packages</b></a>
+						<a class="nav-link packages-tab" data-toggle="tab" ng-click=""><b>Packages</b></a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" data-toggle="tab" href="{{userTypeIndicator}}/reports/permac/macs?mac={{macParam}}"><i class="fa fa-arrow-left" aria-hidden="true" style="margin-top: 3"></i></a>
@@ -55,11 +58,225 @@
 
 	</div>
 
+
+				<div class="ch-package-cont">
+					<style type="text/css">
+						.packages-graph{
+							display: none;
+						}
+						.ch-package-cont{
+							height: auto;
+							width: 95%;
+							margin: 20 auto 0 auto;
+							display:  none;
+						}
+						
+
+						.ch-dispense-frame{
+							width: 100%;
+							margin: auto;
+						}
+						.ch-dispense-cont{
+							margin: auto auto auto 10;
+							width: 60%;
+						}
+						.ch-tdtv-cont{
+							margin: auto;
+							width: 38%;
+						}
+
+						.ch-d-frame{
+							width: 30%;
+							height: 90px;
+							border: 1px solid black;
+							margin: auto auto 20 auto;
+						}
+						.ch-tdtv-frame{
+							width: 70%;
+							height: 90px;
+							border: 1px solid black;
+							margin: auto auto 20 auto;
+						}
+						.ch-d-label{
+							color: white;
+							padding-top: 5;
+							padding-left: 10;
+							font-size: 15pt;
+							font-weight: 600;
+						}
+						.ch-d-value{
+							padding-left: 10;
+							padding-top: 10;
+							font-size: 13pt;
+							font-weight: 600;
+						}
+					</style>
+					
+					
+					
+
+					<div class="row ch-dispense-frame">
+						<div class="ch-dispense-cont">
+							<div class="row">
+								<div class="ch-d-frame" style="background-color: rgba(14, 16, 15, 0.4); border: 1px solid rgba(14, 16, 15, 0.9);">
+									<div class="ch-d-label">
+										<font>30Mins</font>
+									</div>
+									<div class="ch-d-value">
+										<font>{{xxxMinutes}}</font>
+									</div>
+								</div>
+								<div class="ch-d-frame" style="background-color: rgba(35, 255, 86, 0.3); border: 1px solid rgba(0, 204, 47, 0.76);">
+									<div class="ch-d-label">
+										<font>1Hr</font>
+									</div>
+									<div class="ch-d-value">
+										<font>{{iHour}}</font>
+									</div>
+								</div>
+								<div class="ch-d-frame" style="background-color: rgba(103, 245, 253, 0.3); border: 1px solid rgba(0, 198, 179, 0.76);">
+									<div class="ch-d-label">
+										<font>2Hrs</font>
+									</div>
+									<div class="ch-d-value">
+										<font>{{iiHours}}</font>
+									</div>
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="ch-d-frame" style="background-color: rgba(255, 35, 35, 0.3); border: 1px solid rgba(198, 0, 0, 0.76);">
+									<div class="ch-d-label">
+										<font>3Hrs</font>
+									</div>
+									<div class="ch-d-value">
+										<font>{{iiiHours}}</font>
+									</div>
+								</div>
+								<div class="ch-d-frame" style="background-color: rgba(255, 174, 35, 0.3); border: 1px solid rgba(204, 129, 0, 0.76);">
+									<div class="ch-d-label">
+										<font>5Hrs</font>
+									</div>
+									<div class="ch-d-value">
+										<font>{{vHours}}</font>
+									</div>
+								</div>
+								<div class="ch-d-frame" style="background-color: rgba(255, 40, 169, 0.3); border: 1px solid rgba(204, 0, 122, 0.76);">
+									<div class="ch-d-label">
+										<font>1Day</font>
+									</div>
+									<div class="ch-d-value">
+										<font>{{iDay}}</font>
+									</div>
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="ch-d-frame" style="background-color: rgba(93, 0, 193, 0.5); border: 1px solid rgba(76, 0, 158, 0.9);">
+									<div class="ch-d-label">
+										<font>2Days</font>
+									</div>
+									<div class="ch-d-value">
+										<font>{{iiDays}}</font>
+									</div>
+								</div>
+								<div class="ch-d-frame" style="background-color: rgba(44, 116, 251, 0.3); border: 1px solid rgba(0, 71, 204, 0.76);">
+									<div class="ch-d-label">
+										<font>4Days</font>
+									</div>
+									<div class="ch-d-value">
+										<font>{{ivDays}}</font>
+									</div>
+								</div>
+								<div class="ch-d-frame" style="background-color: rgba(170, 35, 255, 0.3); border: 1px solid rgba(100, 0, 163, 0.76);">
+									<div class="ch-d-label">
+										<font>1Week</font>
+									</div>
+									<div class="ch-d-value">
+										<font>{{iWeek}}</font>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="ch-tdtv-cont">
+							<div class="ch-tdtv-frame" style="background-color: rgba(14, 16, 15, 0.4); border: 1px solid rgba(14, 16, 15, 0.9);">
+								<div class="ch-d-label">
+									<font>Total Package</font>
+								</div>
+								<div class="ch-d-value">
+									<font>{{totalPackage}}</font>
+								</div>
+							</div>
+							<div class="ch-tdtv-frame" style="background-color: rgba(170, 35, 255, 0.3); border: 1px solid rgba(100, 0, 163, 0.76);">
+								<div class="ch-d-label">
+									<font>Total Dispensed</font>
+								</div>
+								<div class="ch-d-value">
+									<font>{{totalDispense}}</font>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+
+
+
+
+
+
+
+					<div class="row row1">
+
+						<div class="chart-container2" style="border-top: 2px solid rgba(14, 16, 15, 0.9);">
+							<div class="row">
+								<h5>Total Package</h5>
+								<ul class="nav nav-tabs chart-tab">
+								  <li class="nav-item">
+								    <a class="nav-link tp-line" data-toggle="tab" href="#">Line</a>
+								  </li>
+								  <li class="nav-item">
+								    <a class="nav-link tp-bar" data-toggle="tab" href="#">Bar</a>
+								  </li>
+								</ul>
+							</div>
+							<div class="canvas-container">
+								<canvas id="canvas39"></canvas>
+								<canvas id="canvas40"></canvas>
+							</div>
+						</div>
+						
+						<div class="chart-container2" style="border-top: 2px solid rgba(100, 0, 163, 0.76);">
+							<div class="row">
+								<h5>Total Dispense</h5>
+								<ul class="nav nav-tabs chart-tab">
+								  <li class="nav-item">
+								    <a class="nav-link td-line" data-toggle="tab" href="#">Line</a>
+								  </li>
+								  <li class="nav-item">
+								    <a class="nav-link td-bar" data-toggle="tab" href="#">Bar</a>
+								  </li>
+								</ul>
+							</div>
+							<div class="canvas-container">
+								<canvas id="canvas41"></canvas>
+								<canvas id="canvas42"></canvas>
+							</div>
+						</div>
+
+						
+
+					</div>
+
+				</div>
+
+
 	<div class="stats-graph">
 		<div class="row row1">
 			<div class="chart-container chart-active-container" style="border-top: 2px solid rgba(204, 0, 122, 0.76);">
 				<div class="row">
-					<h5>Actives ({{trend}})</h5>
+					<h5>Active</h5>
 					<ul class="nav nav-tabs chart-tab">
 					  <li class="nav-item">
 					    <a class="nav-link active-line" data-toggle="tab" href="#">Line</a>
@@ -74,9 +291,10 @@
 					<canvas id="canvas2"></canvas>
 				</div>
 			</div>
+			
 			<div class="chart-container chart-connected-container" style="border-top: 2px solid rgba(76, 0, 158, 0.9);">
 				<div class="row">
-					<h5>Connected ({{trend}})</h5>
+					<h5>Connected</h5>
 					<ul class="nav nav-tabs chart-tab">
 					  <li class="nav-item">
 					    <a class="nav-link connected-line" data-toggle="tab" href="#">Line</a>
@@ -91,12 +309,10 @@
 					<canvas id="canvas4"></canvas>
 				</div>
 			</div>
-		</div>
 
-		<div class="row row1">
 			<div class="chart-container" style="border-top: 2px solid rgba(14, 16, 15, 0.9);">
 				<div class="row">
-					<h5>Ccq ({{trend}})</h5>
+					<h5>Ccq</h5>
 					<ul class="nav nav-tabs chart-tab">
 					  <li class="nav-item">
 					    <a class="nav-link ccq-line" data-toggle="tab" href="#">Line</a>
@@ -111,29 +327,10 @@
 					<canvas id="canvas6"></canvas>
 				</div>
 			</div>
-			<div class="chart-container" style="border-top: 2px solid rgba(0, 71, 204, 0.76);">
-				<div class="row">
-					<h5>Util ({{trend}})</h5>
-					<ul class="nav nav-tabs chart-tab">
-					  <li class="nav-item">
-					    <a class="nav-link util-line" data-toggle="tab" href="#">Line</a>
-					  </li>
-					  <li class="nav-item">
-					    <a class="nav-link util-bar" data-toggle="tab" href="#">Bar</a>
-					  </li>
-					</ul>
-				</div>
-				<div class="canvas-container">
-					<canvas id="canvas7"></canvas>
-					<canvas id="canvas8"></canvas>
-				</div>
-			</div>
-		</div>
 
-		<div class="row row1">
 			<div class="chart-container" style="border-top: 2px solid rgba(100, 0, 163, 0.76)">
 				<div class="row">
-					<h5>Usage ({{trend}})</h5>
+					<h5>Usage</h5>
 					<ul class="nav nav-tabs chart-tab">
 					  <li class="nav-item">
 					    <a class="nav-link usage-line" data-toggle="tab" href="#">Line</a>
@@ -148,9 +345,32 @@
 					<canvas id="canvas10"></canvas>
 				</div>
 			</div>
+
+		</div>
+
+		<div class="row row1">
+
+			<div class="chart-container" style="border-top: 2px solid rgba(0, 71, 204, 0.76);">
+				<div class="row">
+					<h5>Util</h5>
+					<ul class="nav nav-tabs chart-tab">
+					  <li class="nav-item">
+					    <a class="nav-link util-line" data-toggle="tab" href="#">Line</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link util-bar" data-toggle="tab" href="#">Bar</a>
+					  </li>
+					</ul>
+				</div>
+				<div class="canvas-container">
+					<canvas id="canvas7"></canvas>
+					<canvas id="canvas8"></canvas>
+				</div>
+			</div>
+			
 			<div class="chart-container" style="border-top: 2px solid rgba(0, 204, 47, 0.76);">
 				<div class="row">
-					<h5>Lease ({{trend}})</h5>
+					<h5>Lease</h5>
 					<ul class="nav nav-tabs chart-tab">
 					  <li class="nav-item">
 					    <a class="nav-link lease-line" data-toggle="tab" href="#">Line</a>
@@ -165,12 +385,10 @@
 					<canvas id="canvas12"></canvas>
 				</div>
 			</div>
-		</div>
 
-		<div class="row row1">
 			<div class="chart-container" style="border-top: 2px solid rgba(0, 198, 179, 0.76);">
 				<div class="row">
-					<h5>Free Mem ({{trend}})</h5>
+					<h5>Free Mem</h5>
 					<ul class="nav nav-tabs chart-tab">
 					  <li class="nav-item">
 					    <a class="nav-link freeMem-line" data-toggle="tab" href="#">Line</a>
@@ -185,9 +403,14 @@
 					<canvas id="canvas14"></canvas>
 				</div>
 			</div>
+
+		</div>
+
+		<div class="row row1">
+			
 			<div class="chart-container" style="border-top: 2px solid rgba(204, 129, 0, 0.76);">
 				<div class="row">
-					<h5>CPU Freq ({{trend}})</h5>
+					<h5>CPU Freq</h5>
 					<ul class="nav nav-tabs chart-tab">
 					  <li class="nav-item">
 					    <a class="nav-link cpuFreq-line" data-toggle="tab" href="#">Line</a>
@@ -202,12 +425,10 @@
 					<canvas id="canvas16"></canvas>
 				</div>
 			</div>
-		</div>
 
-		<div class="row row1">
 			<div class="chart-container" style="border-top: 2px solid rgba(198, 0, 0, 0.76);">
 				<div class="row">
-					<h5>CPU Load ({{trend}})</h5>
+					<h5>CPU Load</h5>
 					<ul class="nav nav-tabs chart-tab">
 					  <li class="nav-item">
 					    <a class="nav-link cpuLoad-line" data-toggle="tab" href="#">Line</a>
@@ -224,7 +445,7 @@
 			</div>
 			<div class="chart-container" style="border-top: 2px solid rgba(11, 29, 24, 0.76);">
 				<div class="row">
-					<h5>Free Hdd ({{trend}})</h5>
+					<h5>Free Hdd</h5>
 					<ul class="nav nav-tabs chart-tab">
 					  <li class="nav-item">
 					    <a class="nav-link freeHdd-line" data-toggle="tab" href="#">Line</a>
@@ -239,7 +460,9 @@
 					<canvas id="canvas20"></canvas>
 				</div>
 			</div>
+
 		</div>
+
 	</div>
 
 
@@ -250,29 +473,10 @@
 
 
 	<div class="packages-graph">
-		<div class="row row1">
-			<div class="chart-container" style="border-top: 4px solid rgba(0, 71, 204, 0.76); width: 90%; height: 400px; margin:auto;">
-				<div class="row">
-					<h5>Packages</h5>
-					<ul class="nav nav-tabs chart-tab">
-					  <li class="nav-item">
-					    <a class="nav-link packages-line" data-toggle="tab" href="#">Line</a>
-					  </li>
-					  <li class="nav-item">
-					    <a class="nav-link packages-bar" data-toggle="tab" href="#">Bar</a>
-					  </li>
-					</ul>
-				</div>
-				<div class="canvas-container" style="height: 300px">
-					<canvas id="canvas37"></canvas>
-					<canvas id="canvas38"></canvas>
-				</div>
-			</div>
-			
-		</div>
+		
 
 		<div class="row row1">
-			<div class="chart-container" style="border-top: 4px solid rgba(0, 71, 204, 0.76);">
+			<div class="chart-container" style="border-top: 2px solid rgba(14, 16, 15, 0.9);">
 				<div class="row">
 					<h5>30Mins</h5>
 					<ul class="nav nav-tabs chart-tab">
@@ -289,7 +493,7 @@
 					<canvas id="canvas22"></canvas>
 				</div>
 			</div>
-			<div class="chart-container" style="border-top: 4px solid rgba(100, 0, 163, 0.76);">
+			<div class="chart-container" style="border-top: 2px solid rgba(0, 204, 47, 0.76);">
 				<div class="row">
 					<h5>1Hour</h5>
 					<ul class="nav nav-tabs chart-tab">
@@ -306,10 +510,7 @@
 					<canvas id="canvas24"></canvas>
 				</div>
 			</div>
-		</div>
-
-		<div class="row row1">
-			<div class="chart-container" style="border-top: 4px solid rgba(0, 71, 204, 0.76);">
+			<div class="chart-container" style="border-top: 2px solid rgba(0, 198, 179, 0.76);">
 				<div class="row">
 					<h5>2Hours</h5>
 					<ul class="nav nav-tabs chart-tab">
@@ -326,7 +527,28 @@
 					<canvas id="canvas26"></canvas>
 				</div>
 			</div>
-			<div class="chart-container" style="border-top: 4px solid rgba(100, 0, 163, 0.76);">
+		</div>
+
+		<div class="row row1">
+			
+			<div class="chart-container" style="border-top: 2px solid rgba(198, 0, 0, 0.76);">
+				<div class="row">
+					<h5>3Hours</h5>
+					<ul class="nav nav-tabs chart-tab">
+					  <li class="nav-item">
+					    <a class="nav-link 3hours-line" data-toggle="tab" href="#">Line</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link 3hours-bar" data-toggle="tab" href="#">Bar</a>
+					  </li>
+					</ul>
+				</div>
+				<div class="canvas-container">
+					<canvas id="canvas37"></canvas>
+					<canvas id="canvas38"></canvas>
+				</div>
+			</div>
+			<div class="chart-container" style="border-top: 2px solid rgba(204, 129, 0, 0.76);">
 				<div class="row">
 					<h5>5Hours</h5>
 					<ul class="nav nav-tabs chart-tab">
@@ -343,10 +565,7 @@
 					<canvas id="canvas28"></canvas>
 				</div>
 			</div>
-		</div>
-
-		<div class="row row1">
-			<div class="chart-container" style="border-top: 4px solid rgba(0, 71, 204, 0.76);">
+			<div class="chart-container" style="border-top: 2px solid rgba(204, 0, 122, 0.76);">
 				<div class="row">
 					<h5>1Day</h5>
 					<ul class="nav nav-tabs chart-tab">
@@ -363,7 +582,11 @@
 					<canvas id="canvas30"></canvas>
 				</div>
 			</div>
-			<div class="chart-container" style="border-top: 4px solid rgba(100, 0, 163, 0.76);">
+		</div>
+
+		<div class="row row1">
+			
+			<div class="chart-container" style="border-top: 2px solid rgba(76, 0, 158, 0.9);">
 				<div class="row">
 					<h5>2Days</h5>
 					<ul class="nav nav-tabs chart-tab">
@@ -380,10 +603,8 @@
 					<canvas id="canvas32"></canvas>
 				</div>
 			</div>
-		</div>
 
-		<div class="row row1">
-			<div class="chart-container" style="border-top: 4px solid rgba(0, 71, 204, 0.76);">
+			<div class="chart-container" style="border-top: 2px solid rgba(0, 71, 204, 0.76);">
 				<div class="row">
 					<h5>4Days</h5>
 					<ul class="nav nav-tabs chart-tab">
@@ -400,7 +621,7 @@
 					<canvas id="canvas34"></canvas>
 				</div>
 			</div>
-			<div class="chart-container" style="border-top: 4px solid rgba(100, 0, 163, 0.76);">
+			<div class="chart-container" style="border-top: 2px solid rgba(100, 0, 163, 0.76);">
 				<div class="row">
 					<h5>1Week</h5>
 					<ul class="nav nav-tabs chart-tab">
@@ -417,6 +638,10 @@
 					<canvas id="canvas36"></canvas>
 				</div>
 			</div>
+		</div>
+
+		<div class="row row1">
+			
 		</div>
 	</div>
 </div>
