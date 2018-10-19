f975bddb31992757b15ce360a667a06ac309981d
DexterDiolola <dexterdiolola@gmail.com>
2018-10-19 15:58:41 +0800

    Update UserLoginController.php

--- macStat/app/controllers/User/UserLoginController.php
+++ macStat/app/controllers/User/UserLoginController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Auth;
 use App\User;
+use DB;
 
 class UserLoginController extends Controller
 {
@@ -23,8 +24,14 @@ class UserLoginController extends Controller
     public function register(Request $request){
         $this->validateRegister($request);
         $request['password'] = bcrypt($request->password);
+        $request['userType'] = 'Operator';
+
+        // The newly created user/operator must be inserted in partners table
+        $operator = $request->username;
+        DB::select("call mac_administration('add-new-operator', '$operator', '', '')");
+
         User::create($request->all());
-        return redirect('/user/login');
+        return redirect('/operator/login');
     }    
 
     public function showLoginForm(){
@@ -33,15 +40,28 @@ class UserLoginController extends Controller
 
     public function login(Request $request){
         $this->validateLogin($request);
-         if(Auth::attempt(['username'=>$request->username, 'password'=>$request->password])){
-            return redirect('user/dashboard');
+         if(Auth::attempt(['username'=>$request->username, 
+                           'password'=>$request->password])){
+            
+            Auth::guard('web')->logout();
+            $userName = $request->username;
+            $userType = DB::select("call mac_administration('get-user-type', '$userName', '', '')");
+            $userType = json_decode(json_encode($userType),true);
+
+            if($userType[0]['userType'] == 'Operator'){
+                Auth::attempt(['username' => $request->username,
+                               'password' => $request->password]);
+            }
+            
+            return redirect('operator/login')->withErrors(['Login Failed User Unknown']); 
+
          }
-         return redirect('user/login')->withErrors(['Login Failed']);
+         return redirect('operator/login')->withErrors(['Login Failed']);
     }
 
     public function logout(){
         Auth::guard('web')->logout();
-        return redirect('/user/dashboard');  /*To prevent the use of back button, 
+        return redirect('/operator/dashboard');  /*To prevent the use of back button, 
                                          redirect it to your dashboard page when it is 
                                          logged out*/
     }
